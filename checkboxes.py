924a896950bb3951718b14d7e87c92443618b50a
satverm <64876484+satverm@users.noreply.github.com>
2022-10-09 15:14:22 +0530

    updated with 2 check boxes

--- checkboxes.py
+++ checkboxes.py
@@ -7,16 +7,22 @@ root.iconbitmap("my_icon.ico")
 root.geometry("700x500")
 
 def show():
-    my_label = Label(root, text= var.get()).pack()
+    my_label = Label(root, text= var_1.get()).pack()
+    my_labe2 = Label(root, text= var_2.get()).pack()
 
-# var = IntVar()  # depends on whar you use 1/0 or On/Off
+# var = IntVar()  # can be used when you use 1/0 
 
-var = StringVar()
+var_1 = StringVar()
+c_btn_1 = Checkbutton(root,text="Click to Select", variable=var_1, onvalue="Selected", offvalue="Not Selected") # we can use any string for onvalue and offvalue
+c_btn_1.deselect()
+c_btn_1.pack()
 
-c_btn = Checkbutton(root,text="Click to Select", variable=var, onvalue="Selected", offvalue="Not Selected") # we can use any string for onvalue and offvalue
-c_btn.deselect()
+# check button 2
+var_2 = StringVar()
+c_btn_2 = Checkbutton(root,text="Click to Select", variable=var_2, onvalue="Selected", offvalue="Not Selected") # we can use any string for onvalue and offvalue
+c_btn_2.deselect()
+c_btn_2.pack()
 
-c_btn.pack()
 
 myButton = Button(root, text= "Show Selection", command=show).pack()
 
