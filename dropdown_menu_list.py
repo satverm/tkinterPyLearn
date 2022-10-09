455c600242e7e3381dde3023242b697c6888cb90
satverm <64876484+satverm@users.noreply.github.com>
2022-10-09 15:43:14 +0530

    updated dropdown box wiht list

---
+++ dropdown_menu_list.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Drop down menu using list")
+root.iconbitmap("my_icon.ico")
+
+def show():
+    seleced_label = Label(root, text= var_1.get()).pack()
+
+options_list = ["item_1","item_2","item_3","item_4","item_5"]    
+
+var_1 = StringVar()
+var_1.set("default_sel")
+opt_btn = OptionMenu(root, var_1,*options_list)  # * is used to open the list items as arguments
+
+opt_btn.pack()
+
+button_show = Button(root, text= "Show selection", command= show).pack()
+
+
+
+root.mainloop()
