35e2f335213381f09607be7506c726acd98e1c18
satverm <64876484+satverm@users.noreply.github.com>
2022-10-09 15:02:09 +0530

    updated checkbutton

--- checkboxes.py
+++ checkboxes.py
@@ -9,8 +9,13 @@ root.geometry("700x500")
 def show():
     my_label = Label(root, text= var.get()).pack()
 
-var = IntVar()
-c_btn = Checkbutton(root,text="Click to Select", variable=var)
+# var = IntVar()  # depends on whar you use 1/0 or On/Off
+
+var = StringVar()
+
+c_btn = Checkbutton(root,text="Click to Select", variable=var, onvalue="Selected", offvalue="Not Selected") # we can use any string for onvalue and offvalue
+c_btn.deselect()
+
 c_btn.pack()
 
 myButton = Button(root, text= "Show Selection", command=show).pack()
