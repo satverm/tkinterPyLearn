eb9304ef7016dd7ca0c08871fe0b5def8651775d
satverm <64876484+satverm@users.noreply.github.com>
2022-10-11 00:29:54 +0530

    updated

--- db_tk.py
+++ db_tk.py
@@ -1,3 +1,5 @@
+from cgi import print_directory
+from cgitb import text
 from tkinter import *
 from PIL import ImageTk, Image
 import sqlite3
@@ -26,9 +28,9 @@ def append_data():
 
     conn = sqlite3.connect('project_file.db')
     cur = conn.cursor()
-    cur.execute("INSERT INTO project VALUES (:project_name,:val_2,:val_3, :val_4, :val_5)",
+    cur.execute("INSERT INTO project VALUES (:val_1,:val_2,:val_3, :val_4, :val_5)",
             {
-                'project_name': project_name.get(),
+                'val_1': project_name.get(),
                 'val_2': project_type.get(),
                 'val_3': project_cost.get(),
                 'val_4': project_head.get(),
@@ -43,6 +45,22 @@ def append_data():
     project_head.delete(0, END)
     project_duration_months.delete(0, END)
 
+def show_rec():
+    conn = sqlite3.connect('project_file.db')
+    cur = conn.cursor()
+    cur.execute("SELECT *, oid FROM project")
+    records= cur.fetchall()
+    print(records)
+    print_records = ""
+    for item in records:
+        print_records += str(item) + "\n"
+    
+    records_label = Label(root, text=print_records)
+    records_label.grid(row=8, column=0, columnspan=2)
+       
+    conn.commit()
+    conn.close()
+    pass
 
 project_name = Entry(root, width=30)
 project_name.grid(row=0, column=1)
@@ -72,6 +90,9 @@ project_duration_months_label.grid(row=4, column=0)
 submit_button = Button(root, text="Add the data in the database",command= append_data)
 submit_button.grid(row=6, column=0, columnspan=2, pady=5, padx=5, ipadx=170)
 
+# button to test if data has been updated
+query_btn = Button(root, text="Show reconds", command=show_rec)
+query_btn.grid(row=7, column=0, columnspan=2, padx=10, pady=10, ipadx=170)
 
 
 
