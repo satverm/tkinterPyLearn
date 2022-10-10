261ebe9ca56d39eaf596cc1fc37fc030178fc0db
satverm <64876484+satverm@users.noreply.github.com>
2022-10-10 23:18:48 +0530

    updated db_tk

--- db_tk.py
+++ db_tk.py
@@ -8,7 +8,57 @@ root.iconbitmap("my_icon.ico")
 root.geometry("500x500")
 
 
+# Creating database
+conn = sqlite3.connect('project_file.db')
+# create a cursor
+cur = conn.cursor()
+cur.execute('''CREATE TABLE  IF NOT EXISTS project (
+    project_name text,
+    project_type text,
+    project_cost intiger,
+    project_head text,
+    project_duration_months intiger
+    )''')
 
+def append_data():
+    project_name.delete(0, END)
+    project_type.delete(0, END)
+    project_cost.delete(0, END)
+    project_head.delete(0, END)
+    project_duration_months.delete(0, END)
+
+
+project_name = Entry(root, width=30)
+project_name.grid(row=0, column=1)
+project_type = Entry(root, width=30)
+project_type.grid(row=1, column=1)
+project_cost = Entry(root, width=30)
+project_cost.grid(row=2, column=1)
+project_head = Entry(root, width=30)
+project_head.grid(row=3, column=1 )
+project_duration_months  = Entry(root, width=30)
+project_duration_months.grid(row=4, column=1)
+
+# Now let's create the labels for these items
+
+project_name_label = Label(root,text="Project Name", width=30)
+project_name_label.grid(row=0, column=0)
+project_type_label = Label(root,text= "Project Type", width=30)
+project_type_label.grid(row=1, column=0)
+project_cost_label = Label(root,text= "Project Cost", width=30)
+project_cost_label.grid(row=2, column=0)
+project_head_label = Label(root, text= "Project Head", width=30)
+project_head_label.grid(row=3, column=0 )
+project_duration_months_label  = Label(root, text= "Project Duration", width=30)
+project_duration_months_label.grid(row=4, column=0)
+
+# submit buttons for updating the database
+submit_button = Button(root, text="Add the data in the database",command= append_data)
+submit_button.grid(row=6, column=0, columnspan=2, pady=5, padx=5, ipadx=170)
+
+conn.commit()
+
+conn.close()
 
 
 
