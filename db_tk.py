5ba150eefb52124f2c8bcc4a14a2da23fe7f746b
satverm <64876484+satverm@users.noreply.github.com>
2022-10-10 20:11:35 +0530

    database_tk

---
+++ db_tk.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import sqlite3
+
+root = Tk()
+root.title("Database with Tk")
+root.iconbitmap("my_icon.ico")
+root.geometry("500x500")
+
+
+
+
+
+
+
+root.mainloop()
